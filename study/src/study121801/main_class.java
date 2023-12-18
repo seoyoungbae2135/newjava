518fff01479e136715a624f133e3fd422445d9f3
seoyoungbae2135 <ybsho62@naver.com>
2023-12-18 10:28:01 +0900

    sss

--- study/src/study121801/main_class.java
+++ study/src/study121801/main_class.java
@@ -1,7 +1,14 @@
 package study121801;
 
 public class main_class {
+	
 	public static void main(String[] args) {
 		
+		// 자바슼립트 - let a = new Array();
+		//           let b = [10,20,30,40];
+		//			a.push(3123); b.push(2330
+		
+		int[] arr1 = {102, 20};
+		System.out.println( arr1[0]);
 	}
 }
