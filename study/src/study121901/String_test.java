ee65a1e80d7c1f9493fb55421dc89f647d1d0a47
seoyoungbae2135 <ybsho62@naver.com>
2023-12-19 12:53:33 +0900

    1219과제

--- study/src/study121901/String_test.java
+++ study/src/study121901/String_test.java
@@ -34,8 +34,22 @@ public class String_test {
 		String fruit = song.substring(song.indexOf(" ")+1, song.indexOf("알러지"));
 		System.out.println(fruit);
 		
-		temp = "상상만 해도 웃음이 나요 ";
+		temp = "상상만 해도 웃음이 나요";
 		System.out.println(temp.concat(song));
+		
+		// 과제
+		song = "크리스마스에는 많은것을 원하지 않아요 # 내가 원하는건 딱 하나예요 #"
+				+ "트리 아래의 선물같은것도 신경쓰지 않아요 # 당신을 원해요 #"
+				+ "당신이 생각하는 것보다 더 # 내소원을 이뤄줘요 #"
+				+ "내가 크리스마스에 원하는건 당신이예요";
+		System.out.println(song);
+				 
+		/*
+		 *1. #기준으로 한줄씩 출력하세요(#은 출력창에 나오면 안되요)
+		 *2. 당신 이라는 단어 대신 "그대" 라고 바꾸어서 출력하세요
+		 *3. 크리스마스를 영어로 번역하여 출력하세요(대문자로)
+		 *4. 세번째#뒤의 가사가 무었인지 출력하세요.
+		 */
 		
 
 	}
